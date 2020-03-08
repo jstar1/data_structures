a6a4bba6b620372974c8ad2c4d18f605eeb1eea8
Justin Moore <juswmoore@gmail.com>
2020-03-08 05:34:12 -0700

    dynamic_array: Init low level array
    Initialize low level array using pyobject

--- dynamic_array/array.py
+++ dynamic_array/array.py
@@ -6,8 +6,11 @@ import ctypes
 class DynamicArray:
     def __init__(self):
         self._n = 0 # count of actual elements
-        self.capacity = 1 # default arracy capacity
+        self._capacity = 1 # default arracy capacity
         self.dynamic_array = self._make_dynamic_array(self._capacity) # init array
 
-    def _make_dynamic_array(self, capcaity):
+    def  __len__(self):
+        return self._n
+
+    def _make_dynamic_array(self, capacity):
         return (capacity * ctypes.py_object)()
--- test/test_dynamic_array.py
+++ test/test_dynamic_array.py
@@ -1,11 +1,15 @@
 #!/usr/bin/python3
 
 import unittest
-from array import DynamicArray
+from dynamic_array.array import DynamicArray
 
 
 class TestDynamicArray(unittest.TestCase):
     def setUp(self):
         self.array = DynamicArray()
 
-    
+    def test_len(self):
+        self.assertEqual(len(self.array) == 0, True)
+
+if __name__ == '__main__':
+    unittest.main()    
