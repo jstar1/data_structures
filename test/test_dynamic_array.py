0f7ea1c4851d16542a386fd181f9ddffc78f9356
Justin Moore <juswmoore@gmail.com>
2020-03-08 05:22:55 -0700

    Test: Init test commit

---
+++ test/test_dynamic_array.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+import unittest
+from array import DynamicArray
+
+
+class TestDynamicArray(unittest.TestCase):
+    def setUp(self):
+        self.array = DynamicArray()
+
+    
