fe7e798224aa495c37e8691b0c885a10d495d58a
Justin Moore <juswmoore@gmail.com>
2020-03-10 03:57:11 -0700

    dynamic array: Add more testing

--- dynamic_array/array.py
+++ dynamic_array/array.py
@@ -5,59 +5,66 @@ import ctypes
 
 class DynamicArray:
     def __init__(self):
-        self._n = 0 # count of actual elements
-        self._capacity = 1 # default arracy capacity
-        self._dynamic_array = self._make_dynamic_array(self._capacity) # init array
+        self._n = 0  # count of actual elements
+        self._capacity = 1  # default arracy capacity
+        self._dynamic_array = self._make_dynamic_array(self._capacity)  # init array
 
-    def  __len__(self):
+    def __len__(self):
         return self._n
-    
+
     def __getitem__(self, k):
         if not 0 <= k < self._n:
-            raise IndexError('invalid index')
+            raise IndexError("invalid index")
         return self._dynamic_array[k]
+
+    def __str__(self):
+        return str(self.dynamic_array[: self.__len__])
 
     def append(self, obj):
         if self._n == self._capacity:
             self._resize(2 * self._capacity)
         self._dynamic_array[self._n] = obj
         self._n += 1
-    
+
+    def extend(self, iterable=[]):
+        for ele in iterable:
+            self.append(ele)
+
     def _resize(self, new_capacity):
         bigger_array = self._make_dynamic_array(new_capacity)
         for i in range(self._n):
-            bigger_array[i] = self.dynamic_array[i]
+            bigger_array[i] = self._dynamic_array[i]
         self._dynamic_array = bigger_array
-        self._capacity = new_capcaity    
+        self._capacity = new_capacity
 
     def pop(self, pos=None):
         if len(self._dynamic_array) == 0:
-            raise IndexError('pop from empty list')
+            raise IndexError("pop from empty list")
         del_val = None
-        if pos == None or pos == self._n  - 1:
+        if pos is None or pos == self._n - 1:
             del_val = self._dynamic_array[self._n - 1]
-            self._dynamic_array[self._n - 1] == None
+            self._dynamic_array[self._n - 1] is None
             self._n -= 1
             return del_val
         if 0 <= pos < self._n:
             for j in range(pos, self._n - 1):
-                if j == pos:      
-                    del_val = self._dyanmic_array[j]
-                self._dynamic_array[j] = self._dynamic_array[j+1]
-            self.dynamic_array[self._n - 1] = None
+                if j == pos:
+                    del_val = self._dynamic_array[j]
+                self._dynamic_array[j] = self._dynamic_array[j + 1]
+            self._dynamic_array[self._n - 1] = None
             self._n -= 1
-            return del_val 
-        raise IndexError('invalid index')
-    
+            return del_val
+        raise IndexError("invalid index")
+
     def remove(self, value):
         for i in range(self._n):
             if self._dynamic_array[i] == value:
                 for j in range(i, self._n - 1):
-                    self._dynamic_array[j] = self._dynamic_array[j+1]
+                    self._dynamic_array[j] = self._dynamic_array[j + 1]
                 self._dynamic_array[self._n - 1] = None
                 self._n -= 1
                 return
-        raise ValueError('value not found')
+        raise ValueError("value not found")
 
     def _make_dynamic_array(self, capacity):
         return (capacity * ctypes.py_object)()
--- test/test_dynamic_array.py
+++ test/test_dynamic_array.py
@@ -14,25 +14,28 @@ class TestDynamicArray(unittest.TestCase):
     def test_getitem(self):
         with self.assertRaises(IndexError) as context:
             self.array[10]
-        self.assertTrue('invalid index' in str(context.exception))
+        self.assertTrue("invalid index" in str(context.exception))
 
-    def  test_append(self):
+    def test_append(self):
         self.array.append(1)
         self.assertEqual(self.array[0] == 1, True)
 
     def test_extend(self):
         self.array.extend([1, 2, 3])
         self.assertEqual(len(self.array) == 3, True)
- 
+
     def test_pop(self):
         self.array.append(1)
         self.assertEqual(self.array.pop() == 1, True)
 
     def test_pop_failure(self):
         with self.assertRaises(IndexError) as context:
-           self.array.pop(20)
-        self.assertTrue('invalid index' in str(context.exception)) 
+            self.array.pop(20)
+        self.assertTrue("invalid index" in str(context.exception))
 
+    def test_pop_remove_index(self):
+        self.array.extend([1, 2, 3, 4, 5])
+        self.assertEqual(self.array.pop(3) == 4, True)
 
-if __name__ == '__main__':
-    unittest.main()    
+if __name__ == "__main__":
+    unittest.main()
