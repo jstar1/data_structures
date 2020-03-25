0d2c125108c804083cf19f9173ee89135fdfd510
Justin Moore <juswmoore@gmail.com>
2020-03-25 06:02:00 -0700

    Fixup + circular linked list queue

--- queue/circular_ll_queue.py
+++ queue/circular_ll_queue.py
@@ -1,48 +1,54 @@
 #!/usr/bin/python3
 
 
-class LinkedQueue:
+class CircularLLQueue:
     class _Node:
         __slots__ = '_element', '_next'
-
+        
         def __init__(self, element, next):
             self._element = element
             self._next = next
-        
+
     def __init__(self):
-        self._head = None
         self._tail = None
         self._size = 0
 
     def __len__(self):
         return self._size
-
+    
     def is_empty(self):
         return self._size == 0
 
     def first(self):
-        if self.is_empty():
-            raise Empty('Queue is empty')
-        return self._head._element 
+        if is_empty():
+            return Empty('Queue is empty')
+        head = self._tail._next
+        return head._eleent 
 
     def dequeue(self):
-        if self.is_empty():
-            raise Empty('Queue is empty')
-        answer = self._head._element
-        self._head = self._head._next
-        self._size -= 1
-        if self.is_empty():
+        if is_empty()
+            return Empty('Queue is empty')
+        oldhead = self._tail._next
+        if self._size == 1
             self._tail = None
-        return answer
+        else:
+            self._tail._next = oldhead._next
+        self._size -= 1
+        return oldhead._element
 
     def enqueue(self, value):
         newest = self._Node(value, None)
-        if self._is_empty():
-            self._head = newest
+        if is_empty():
+            newest._next = newest
         else:
+            newest._next = self._tail._next
             self._tail._next = newest
         self._tail = newest
         self._size += 1
+    
+    def rotate(self):
+        if self._size > 0:
+            self._tail = self._tail._next
 
 
 class Empty(Exception):
